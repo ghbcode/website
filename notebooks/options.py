ac09ddfdaddb236b0fc086193466cbc240c289ec
Gonzalo Briceno <gonzalo@lightstreet.com>
2022-06-29 01:18:45 -0700

    add options2 notebook

---
+++ notebooks/options.py
@@ -0,0 +1,241 @@
+import pandas as pd
+import numpy as np
+
+# source: https://clinthoward.github.io/portfolio/2017/04/16/BlackScholesGreeks/
+
+''' Payoff functions
+S = stock underlying
+K = strike price 
+Price = premium paid for option
+''' 
+
+# Simple strategies
+def long_call(S, K, Price):
+    # Long Call Payoff = max(Stock Price - Strike Price, 0)     
+    # If we are long a call, we would only elect to call if the current stock price is greater than    
+    # the strike price on our option     
+    P = list(map(lambda x: max(x - K, 0) - Price, S))
+    return P
+
+def long_put(S, K, Price):
+    # Long Put Payoff = max(Strike Price - Stock Price, 0)     
+    # If we are long a put, we would only elect to call if the current stock price is less than     
+    #the strike price on our option     
+    P = list(map(lambda x: max(K - x,0) - Price, S))
+    return P
+   
+def short_call(S, K, Price):
+    # Payoff a shortcall is just the inverse of the payoff of a long call     
+    P = long_call(S, K, Price)
+    return [-1.0*p for p in P]
+
+def short_put(S,K, Price):
+    # Payoff a short put is just the inverse of the payoff of a long put 
+    P = long_put(S,K, Price)
+    return [-1.0*p for p in P]
+    
+def binary_call(S, K, Price):
+    # Payoff of a binary call is either:     # 1. Strike if current price > strike     # 2. 0     
+    P = list(map(lambda x:  K - Price if x > K else 0 - Price, S))
+    return P
+
+def binary_put(S,K, Price):
+    # Payoff of a binary call is either:     # 1. Strike if current price < strike     # 2. 0     
+    P = list(map(lambda x:  K - Price if x < K else 0 - Price, S))
+    return P 
+
+# More complicated strategies
+def bull_spread(S, E1, E2, Price1, Price2):
+    
+    P_1 = long_call(S, E1, Price1)
+    P_2 = short_call(S, E2, Price2)
+    return [x+y for x,y in zip(P_1, P_2)] 
+     
+def bear_spread(S, E1, E2, Price1, Price2):
+    
+    P = bull_spread(S,E1, E2, Price1, Price2)
+    return [-1.0*p + 1.0 for p in P] 
+
+def straddle(S, E, Price1, Price2):
+    
+    P_1 = long_call(S, E, Price1)
+    P_2 = long_put(S, E, Price2)
+    return [x+y for x,y in zip(P_1, P_2)]
+    
+def risk_reversal(S, E1, E2, Price1, Price2):
+    
+    P_1 = long_call(S, E1, Price1)
+    P_2 = short_put(S,E2, Price2)
+    return [x + y for x, y in zip(P_1, P_2)]
+
+def strangle(S, E1, E2, Price1, Price2):
+    
+    P_1 = long_call(S, E1, Price1)
+    P_2 = long_put(S, E2, Price2)
+    return [x + y for x, y in zip(P_1, P_2)]
+
+
+def butterfly_spread(S, E1, E2, E3, Price1, Price2, Price3):
+    
+    P_1 = long_call(S, E1, Price1)
+    P_2 = long_call(S, E3, Price3)
+    P_3 = short_call(S, E2, Price2)
+    P_3 =[2*p for p in P_3]
+    return [x + y + z for x, y, z in zip(P_1, P_2, P_3)]
+    
+def strip(S, E1, Price1, Price2):
+    
+    P_1 = long_call(S, E1, Price1)
+    P_2 = long_put(S, E1, Price2)
+    P_2 = [2*p for p in P_2]
+    return [x+y for x,y in zip(P_1, P_2)]
+
+
+''' Black-Scholes
+S = stock underlying
+K = strike price 
+r: risk free rate 
+D: dividend value 
+vol: Volatility 
+T: time to expiry (assumed that we're measuring from t=0 to T)
+''' 
+
+from scipy.stats import norm
+
+def d1_calc(S, K, r, vol, T, t):
+    # Calculates d1 in the BSM equation     
+    return (np.log(S/K) + (r + 0.5 * vol**2)*(T-t))/(vol*np.sqrt(T-t))
+
+def BS_call(S, K, r, vol, T, t):
+    d1 = d1_calc(S, K, r, vol, T, t)
+    d2 = d1 - vol * np.sqrt(T)
+    return S * norm.cdf(d1) - K * np.exp(-r*T)*norm.cdf(d2)
+
+def BS_put(S, K, r, vol, T, t):
+    return BS_call(S, K, r, vol, T, t) - S + np.exp(-r*(T-t))*K
+
+def BS_binary_call(S, K, r, vol, T, t):
+    d1 = d1_calc(S, K, r, vol, T, t)
+    d2 = d1 - vol * np.sqrt(T-t)
+    return np.exp(-r*T)*norm.cdf(d2)
+
+def BS_binary_put(S, K, r, vol, T, t):
+    return BS_binary_call(S, K, r, vol, T,t) - S + np.exp(-r*(T-t))*K
+
+# 1st Order Greeks 
+def delta(S, K, r, vol, T, t, otype):
+    d1 = d1_calc(S, K, r, vol, T, t)
+    d2 = d1 - vol * np.sqrt(T-t)
+    
+    if(otype == "call"):
+        delta = np.exp(-(T-t))*norm.cdf(d1)
+    elif(otype == "put"):
+        delta = -np.exp(-(T-t))*norm.cdf(-d1)
+
+    return delta
+
+# Gamma for calls/puts the same 
+def vega(S, K, r, vol, T, t, otype):
+    d1 = d1_calc(S, K, r, vol, T, t)
+    return S * norm.pdf(d1) * np.sqrt(T-t)
+
+def rho(S, K, r, vol, T, t, otype):
+    d1 = d1_calc(S, K, r, vol, T, t)
+    d2 = d1 - vol*np.sqrt(T-t)
+    
+    if(otype == "call"):
+        rho = K*(T-t)*np.exp(-r*(T-t))*norm.cdf(d2)
+    elif(otype == "put"):
+        rho = -K*(T-t)*np.exp(-r*(T-t))*norm.cdf(-d2)
+    return rho
+
+def theta(S, K, r, vol, T, t, otype):
+    d1 = d1_calc(S, K, r, vol, T, t)
+    d2 = d1 - vol*np.sqrt(T-t)
+    
+    if(otype == "call"):
+        theta = -(S*norm.pdf(d1)*vol / (2*np.sqrt(T-t))) - r*K*np.exp(-r*(T-t))*norm.cdf(d2) 
+    elif(otype == "put"):
+        theta = -(S*norm.pdf(d1)*vol / (2*np.sqrt(T-t))) + r*K*np.exp(-r*(T-t))*norm.cdf(-d2)
+
+    return theta
+
+#2nd Order Greeks 
+def gamma(S, K, r, vol, T, t, otype):
+    d1 = d1_calc(S, K, r, vol, T, t)
+    gamma = (norm.pdf(d1)) / (S * vol * np.sqrt(T-t))
+    
+    return gamma
+
+def charm(S, K, r, vol, T, t, otype):
+    d1 = d1_calc(S, K, r, vol, T, t)
+    d2 = d1 - vol*np.sqrt(T-t)
+    charm = -norm.pdf(d1)*(2*r*(T-t) - d2*vol*np.sqrt(T-t))/(2*(T-t)*vol*np.sqrt(T-t))
+
+    return charm
+
+
+'''
+Surfaces
+greek_function: input the function we want to calculate over 
+x_var_name: 1st variable we vary 
+y_var_name: 2nd variable we vary 
+S: vector of underlying prices 
+K: vector of strike prices 
+r: vector of risk free rates 
+vol: vector of volatilities 
+T: vector of expiry  
+t: vector of current date 
+x: meshgrid of 1st variable we vary 
+y: meshgrid of 2nd variable we vary 
+otype: put/call 
+plot: plot figure we want to write to 
+
+'''
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
+def greeks_plot_tool(greek_function, x_var_name, y_var_name,
+                    S, K, r, vol, T, t, x, y, otype, plot):
+   
+    # Initialise vector to store our option values and then iterate over     
+    # Assumption that we're using a constant sized vector length for each variable     
+    # Need to change the variables being iterated over here for each update (possibly a better way to do this)     
+    V = np.zeros((len(S), len(S)), dtype=np.float)
+    for i in range(len(S)):
+        for j in range(len(S)):
+            V[i, j] = greek_function(S[i], K[i], r[i], vol[i], T[j], t[i], otype)
+ 
+    # Initiliase plotting canvas 
+    surf = plot.plot_surface(x, y, V, rstride=1, cstride=1,alpha=0.75,cmap=cm.RdYlBu)
+    plot.set_xlabel('\n' + x_var_name, linespacing=4)
+    plot.set_ylabel('\n' + y_var_name, linespacing=4)
+    plot.set_zlabel("%s(K, T)" % greek_function.__name__)
+    plot.set_title("%s %s" % (otype, greek_function.__name__))
+    
+    # Calculate colour levels based on our meshgrid     
+    Vlevels = np.linspace(V.min(),V.max(),num=8,endpoint=True)
+    xlevels = np.linspace(x.min(),x.max(),num=8,endpoint=True)
+    ylevels = np.linspace(y.min(),y.max(),num=8,endpoint=True)
+    
+    cset = plot.contourf(x, y, V, Vlevels, zdir='z',offset=V.min(),
+                       cmap=cm.RdYlBu,linestyles='dashed')
+    cset = plot.contourf(x, y, V, xlevels, zdir='x',offset=x.min(),
+                       cmap=cm.RdYlBu,linestyles='dashed')
+    cset = plot.contourf(x, y, V, ylevels, zdir='y',offset=y.max(),
+                       cmap=cm.RdYlBu,linestyles='dashed')
+
+    # Set our viewing constraints     
+    for c in cset.collections:
+        c.set_dashes([(0, (2.0, 2.0))]) # Dash contours 
+    plt.clabel(cset,fontsize=10, inline=1)
+    plot.set_xlim(x.min(),x.max())
+    plot.set_ylim(y.min(),y.max())
+    plot.set_zlim(V.min(),V.max())
+
+    # Colorbar     
+    colbar = plt.colorbar(surf, shrink=1.0, extend='both', aspect = 10)
+    l,b,w,h = plt.gca().get_position().bounds
+    ll,bb,ww,hh = colbar.ax.get_position().bounds
+    colbar.ax.set_position([ll, b+0.1*h, ww, h*0.8])
